958f1de1c32ea9f3386204833ee399ca88c4d9b6
Brandon Yuan <brandonyuan05@gmail.com>
2023-10-14 15:42:53 -0500

    skeleton 0.0

---
+++ notes-with-canvas/notes-with-canvas/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  // useState functions here:
+
+  return (
+    <>
+      <div classname = "App">
+        <NavBar/>
+      </div>
+    </>
+  );
+}
+
+export default App
