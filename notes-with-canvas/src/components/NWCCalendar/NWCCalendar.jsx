4abdaa2bfc7c6be616cd4bbd7b3c7717b5bc3908
Brandon Yuan <brandonyuan05@gmail.com>
2024-02-11 13:41:36 -0600

    new sem code

--- notes-with-canvas/src/components/NWCCalendar/NWCCalendar.jsx
+++ notes-with-canvas/src/components/NWCCalendar/NWCCalendar.jsx
@@ -1,20 +1,84 @@
-import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './NWCCalendar.css';
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
+const MyCalendar = withDragAndDrop(Calendar);
 
-export default function NWCCalendar() { 
-    return(
-      <div className="myCustomHeight">
-        <Calendar
+class NWCCalendar extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(2, "hours"),
+        title: "Physics Lab",
+      },
+      {
+        start: moment().add(1, "days"),
+        end: moment().add(1, "days"),
+        title: "Write History Essay",
+      },
+      {
+        start: moment().add(3, "days"),
+        end: moment().add(3, "days"),
+        title: "Finalize Essay",
+      },
+      {
+        start: moment().subtract(10, "days"),
+        end: moment().subtract(10, "days"),
+        title: "Register for classes",
+      },
+      {
+        start: moment().subtract(19, "days"),
+        end: moment().subtract(19, "days"),
+        title: "Calc hw",
+      },
+      {
+        start: moment().subtract(13, "days"),
+        end: moment().subtract(13, "days"),
+        title: "Chemistry lab",
+      },
+      {
+        start: moment().subtract(13, "days"),
+        end: moment().subtract(13, "days"),
+        title: "Do some programming",
+      },
+    ],
+  };
+
+  onEventResize = (data) => {
+    const { start, end } = data;
+
+    this.setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: [...state.events] };
+    });
+  };
+
+  onEventDrop = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <MyCalendar
+          defaultDate={moment().toDate()}
+          defaultView="month"
+          events={this.state.events}
           localizer={localizer}
-          events={myEventsList}
-          startAccessor="start"
-          endAccessor="end"
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "50rem" }}
         />
       </div>
     );
+  }
 }
+
+export default NWCCalendar;
--- notes-with-canvas/src/components/Taskboard/Taskboard.jsx
+++ notes-with-canvas/src/components/Taskboard/Taskboard.jsx
@@ -10,13 +10,33 @@ export default function Taskboard() {
     return(
         <>
             <div className="smaller-title">
-                <h1 className="taskboard-heading">your taskboard:</h1>
+                <h1 className="taskboard-heading">Taskboard:</h1>
             </div>
 
             <div className="taskboard">
                 <Card className="list-card">
-                    <form>
-                        To-Do List #1
+                    <h2 className="list-heading">Todo List #1</h2>
+                    <ul className="ul-list">
+                        <li>Finish math homework</li>
+                        <li>Fix car</li>
+                        <li>Buy new car I guess</li>
+                    </ul>
+                    <form>  
+                        <CardContent className="list-content">
+                            {todo}
+                            <div>
+                            <TextField type="text" className="add-task" onChange={(e) => setTodo(e.target.value)}>
+                                    
+                                    </TextField>
+                                    <button type="submit">add</button>
+                            </div>
+                        </CardContent>
+                    </form>
+                </Card>
+
+                <Card className="list-card">
+                    <h2 className="list-heading">Todo List #2</h2>
+                    <form>  
                         <CardContent className="list-content">
                             {todo}
                             <div>
@@ -28,7 +48,6 @@ export default function Taskboard() {
                         </CardContent>
                     </form>
                 </Card>
-                
             </div>
         </>
     );
